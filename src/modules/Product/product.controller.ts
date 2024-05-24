9acee720bcfabfa9d5ee3808a8834ff91941c1b0
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2024-05-24 19:38:04 +0600

    implemented get all products and get specific product by id method using mongoose

--- src/modules/Product/product.controller.ts
+++ src/modules/Product/product.controller.ts
@@ -1,20 +1,74 @@
 import { Request, Response } from "express"
 import { ProductServices } from "./product.service"
 
+//create product request-response handler
 const createProduct = async (req: Request, res: Response) => {
 
-    const productData = req.body;
-    console.log(productData);
-    
-    const result = await ProductServices.createProduct(productData);
+    try {
+        const productData = req.body;
+        //console.log(productData);
 
-    res.json({
-        success: true,
-        message: 'Product created successfully!',
-        data: result,
-    })
+        const result = await ProductServices.createProduct(productData);
+
+        res.status(200).json({
+            success: true,
+            message: 'Product created successfully!',
+            data: result,
+        })
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error!",
+            error: err,
+        })
+    }
 }
+
+
+//get all products request-response handler
+const getAllProduct = async (req: Request, res: Response) => {
+    try {
+        const result = await ProductServices.getAllProduct()
+
+        res.status(200).json({
+            success: true,
+            message: "Products fetched successfully!",
+            data: result,
+        })
+    } catch (err) {
+        res.status(404).json({
+            success: false,
+            message: "Product not found",
+            error: err,
+        })
+    }
+}
+
+
+//get a single product by id request-response handler
+const getSpecificProduct = async (req: Request, res: Response) => {
+    try {
+        const { productId } = req.params;
+
+        const result = await ProductServices.getSpecificProduct(productId)
+
+        res.status(200).json({
+            success: true,
+            message: "Products fetched successfully!",
+            data: result,
+        })
+    } catch (err) {
+        res.status(404).json({
+            success: false,
+            message: "Product not found",
+            error: err,
+        })
+    }
+}
+
 
 export const ProductControllers = {
     createProduct,
+    getAllProduct,
+    getSpecificProduct,
 }
--- src/modules/Product/product.service.ts
+++ src/modules/Product/product.service.ts
@@ -1,11 +1,26 @@
 import { TProduct } from "./product.interface";
 import { Product } from "./product.model";
 
+//create new product
 const createProduct = async (payload: TProduct) => {
     const result = await Product.create(payload);
     return result;
 }
 
+//get all products
+const getAllProduct = async () => {
+    const result = await Product.find();
+    return result;
+}
+
+//get a single product by id
+const getSpecificProduct = async (_id: string) => {
+    const result = await Product.findOne({_id: _id});
+    return result;
+}
+
 export const ProductServices = {
     createProduct,
+    getAllProduct,
+    getSpecificProduct,
 }
